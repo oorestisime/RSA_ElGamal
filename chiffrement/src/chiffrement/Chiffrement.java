115d91a1fe64cce6eccccbddd0f231041eebf1de
frodoo <oorestisime@gmail.com>
2014-04-03 21:30:59 +0200

    main

--- chiffrement/src/chiffrement/Chiffrement.java
+++ chiffrement/src/chiffrement/Chiffrement.java
@@ -6,6 +6,9 @@
 
 package chiffrement;
 
+import java.math.BigInteger;
+import rsa.*;
+
 /**
  *
  * @author oorestisime
@@ -17,6 +20,10 @@ public class Chiffrement {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        RSA rsa=new RSA(1024);
+        RSA_PlainText plain=new RSA_PlainText(new BigInteger("0123"));
+        RSA_Ciphertext cipher=rsa.encrypt(plain);
+        System.out.println("0123 = "+rsa.decrypt(cipher).getText());
     }
     
 }
